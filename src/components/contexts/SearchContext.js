83c3865ea1772c8f61d878e183768b3d6ce7f84d
Shreyas Patil <102817986+ShreyasPatil-13@users.noreply.github.com>
2024-08-05 02:27:25 +0530

    Add files via upload

---
+++ src/components/contexts/SearchContext.js
@@ -0,0 +1,24 @@
+import React, { createContext, useContext, useState } from "react";
+
+const SearchContext = createContext();
+
+export const SearchProvider = ({ children }) => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [country, setCountry] = useState("IN");
+
+  return (
+    <SearchContext.Provider
+      value={{ searchQuery, setSearchQuery, country, setCountry }}
+    >
+      {children}
+    </SearchContext.Provider>
+  );
+};
+
+export const useSearch = () => {
+  const context = useContext(SearchContext);
+  if (!context) {
+    throw new Error("useSearch must be used within a SearchProvider");
+  }
+  return context;
+};
